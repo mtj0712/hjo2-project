9a2799eae10c810ad8a0977b3ddcb75dabf70cff
Matthew Jo <mtj0712@gmail.com>
2025-07-25 17:14:14 +0900

    .

--- flask_app.py
+++ flask_app.py
@@ -355,11 +355,12 @@ def logout():
         session.pop('id', None)
     if 'username' in session:
         session.pop('username', None)
-    if 'location' in session:
-        session.pop('location', None)
+    if 'location_id' in session:
+        session.pop('location_id', None)
 
     return redirect(url_for('login'))
 
+# TODO: enable location setting in register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     error_msg = ''
@@ -384,7 +385,7 @@ def register():
             hash = password + app.secret_key
             hash = hashlib.sha1(hash.encode())
             password = hash.hexdigest()
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            cursor.execute('INSERT INTO accounts (username, password, email, location_id) VALUES (NULL, %s, %s, %s, %s)', (username, password, email, 1377353))
             mysql.connection.commit()
             error_msg = 'You have successfully registered!'
 
